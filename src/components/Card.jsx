713303ce3685d16fe6b6bae8dccf148a7f20ecd3
DESKTOP-4DDS118\cdjhs <cdjhs310174@ms2.cdjh.hc.edu.tw>
2023-12-01 17:36:52 +0800

    init set up and add ex1

---
+++ src/components/Card.jsx
@@ -0,0 +1,40 @@
+// Card.js
+import React from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  width: 300px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const CardTitle = styled.h2`
+  font-size: 20px;
+  margin-bottom: 8px;
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  border-radius: 4px;
+`;
+
+const CardContent = styled.p`
+  font-size: 16px;
+  color: #555;
+`;
+
+const Card = () => {
+  return (
+    <CardWrapper>
+      <CardTitle>Card Title</CardTitle>
+      <CardImage src="https://fakeimg.pl/350x200/?text=Hello" alt="Card Image" />
+      <CardContent>
+        This is the content of the card. It can contain any information you want to display.
+      </CardContent>
+    </CardWrapper>
+  );
+};
+
+export default Card;
