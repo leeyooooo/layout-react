62081b65704b8ec33f4a8ad95faf31e84e325ed9
dada <davidisdirty@gmail.com>
2023-12-14 02:04:45 +0800

    create Counter.jsx

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Counter = ()=>{
+    return(
+        <>
+        <Layout>
+            <Title></Title>
+        </Layout>
+        
+        
+        <ButtonBar></ButtonBar>
+        </>
+    )
+}
+export default Counter
