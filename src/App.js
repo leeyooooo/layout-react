81d76c8e1ec26606f3d6e2746c28075093b0b1ba
dada <davidisdirty@gmail.com>
2023-12-16 02:08:08 +0800

    title count finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Counter from './components/Counter'
+import Counter from './components/Counter/Counter.jsx'
+
 
 function App() {
   return (
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,47 @@
+const layout = {
+
+
+}
+
+const title = {
+	background: '',
+	color: '',
+	height:'50px',
+	width:'800px',
+	border:'10px solid black',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  margin:'20px',
+  fontSize:'36px',
+  textAlign: 'center'
+}
+
+const count = {
+	background: '',
+	color: '',
+	height:'200px',
+	width:'350px',
+	border:'10px solid black',
+  fontSize: '16px',
+  fontWeight: 'bold',
+	margin:'20px',
+	marginLeft:'250px',
+	fontSize:'36px',
+  textAlign: 'center',
+  lineHeight:'200px'
+}
 const Counter = () => {
+
 	return(
 		<>
-			COUNTER HERE
+		<div style={layout}>
+			<div style={title}>
+				Title
+			</div>
+			<div style={count}>
+				Count
+			</div>
+		</div>
 		</>
 	)
 }
