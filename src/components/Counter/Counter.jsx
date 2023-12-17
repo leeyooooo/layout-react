c3c45d40ed56d8e67ab7c250a868665ce9fed6bd
Liang Ting <liang_ting@Liangdebijixingdiannao.local>
2023-12-17 13:12:10 +0800

    [update] add count button

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,7 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+const CountBtn = styled.button`
+	width: 80px;
+	height: 80px;
+	background: transparent;
+	border: 1px solid #c8c8c8;
+	border-radius: 15px;
+	font-size: 2rem;
+	&:hover {
+		cursor: pointer;
+		background: #c8c8c8;
+		color: #ffffff;
+	}
+`;
+const BtnGroup = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-evenly;
+`;
 const Counter = () => {
 	return(
 		<>
 			COUNTER HERE
+			<BtnGroup>
+				<CountBtn>-</CountBtn>
+				<CountBtn>+</CountBtn>
+			</BtnGroup>
 		</>
 	)
 }
